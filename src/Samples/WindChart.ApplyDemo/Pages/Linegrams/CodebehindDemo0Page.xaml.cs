00a4da332f9d85835d2621b2325ad14766be0fff
deamon-XiaoMi <271884460@qq.com>
2023-08-27 18:14:56 +0800

    添加单线图实时单点更新和一次性刷新的示例

---
+++ src/Samples/WindChart.ApplyDemo/Pages/Linegrams/CodebehindDemo0Page.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WindChart.ApplyDemo.Pages.Linegrams
+{
+    /// <summary>
+    /// CodebehindDemo0Page.xaml 的交互逻辑
+    /// </summary>
+    public partial class CodebehindDemo0Page : Page
+    {
+        public CodebehindDemo0Page()
+        {
+            InitializeComponent();
+
+            // 设置X轴开始显示范围
+            line.XMin = DateTime.Now.ToOADate();
+            line.XMax = DateTime.Now.AddSeconds(60).ToOADate();
+            // 设置X轴刻度文本格式
+            line.XAxisTextFormatString = "HH:mm:ss";
+            // X轴刻度按照日期显示
+            line.IsXAxisTextDateTimeFormat = true;
+            line.IsGraph = false;
+
+            // 轴跟随数据变化
+            line.IsAxisFollowData = true;
+            // Y轴范围
+            line.YMin = 0;
+            line.YMax = 80;
+
+            // 实时数据模拟
+            Task.Run(() =>
+            {
+                Random rand = new Random();
+                double y = 10;
+                while (true)
+                {
+                    var ran = rand.Next(-150, 150);
+                    if (ran > 0)
+                    {
+                        y -= 1;
+                    }
+                    else
+                    {
+                        y += 1;
+                    }
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        line.Add(new Point(DateTime.Now.ToOADate(), y));
+                    });
+                    Thread.Sleep(30);
+                }
+            });
+
+
+            code.Text = @"    // 设置X轴开始显示范围
+    line.XMin = DateTime.Now.ToOADate();
+    line.XMax = DateTime.Now.AddSeconds(60).ToOADate();
+    // 设置X轴刻度文本格式
+    line.XAxisTextFormatString = ""HH: mm:ss"";
+    // X轴刻度按照日期显示
+    line.IsXAxisTextDateTimeFormat = true;
+    line.IsGraph = false;
+
+    // 轴跟随数据变化
+    line.IsAxisFollowData = true;
+    // Y轴范围
+    line.YMin = 0;
+    line.YMax = 80;
+
+    // 实时数据模拟
+    Task.Run(() =>
+    {
+        Random rand = new Random();
+        double y = 10;
+        while (true)
+        {
+            var ran = rand.Next(-150, 150);
+            if (ran > 0)
+            {
+                y -= 1;
+            }
+            else
+            {
+                y += 1;
+            }
+            this.Dispatcher.Invoke(() =>
+            {
+                line.Add(new Point(DateTime.Now.ToOADate(), y));
+            });
+            Thread.Sleep(30);
+        }
+    });";
+
+            xaml.Text= @"<Page x:Class=""WindChart.ApplyDemo.Pages.Linegrams.CodebehindDemo0Page""
+      xmlns = ""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+      xmlns: x = ""http://schemas.microsoft.com/winfx/2006/xaml""
+      xmlns: mc = ""http://schemas.openxmlformats.org/markup-compatibility/2006""
+      xmlns: d = ""http://schemas.microsoft.com/expression/blend/2008""
+      xmlns: local = ""clr-namespace:WindChart.ApplyDemo.Pages.Linegrams""
+      xmlns: windchart = ""clr-namespace:WindChart;assembly=WindChart""
+      mc: Ignorable = ""d""
+      d: DesignHeight = ""450"" d: DesignWidth = ""800""
+      Title = ""CodebehindDemo0Page"" >
+     <Grid>
+        < Border Padding = ""24"" Background = ""WhiteSmoke"" >
+            <windchart:Linegram x:Name = ""line"" />
+         </Border >
+     </Grid >
+ </Page > ";
+        }
+    }
+}
---
+++ src/Samples/WindChart.ApplyDemo/Pages/Linegrams/CodebehindDemo1Page.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WindChart.ApplyDemo.Pages.Linegrams
+{
+    /// <summary>
+    /// CodebehindDemo1Page.xaml 的交互逻辑
+    /// </summary>
+    public partial class CodebehindDemo1Page : Page
+    {
+        public CodebehindDemo1Page()
+        {
+            InitializeComponent();
+
+            // 设置X轴开始显示范围
+            DateTime dtStart = DateTime.Now.AddMinutes(-3);
+            line.XMin = dtStart.ToOADate();
+            line.XMax = DateTime.Now.AddSeconds(60).ToOADate();
+            // 设置X轴刻度文本格式
+            line.XAxisTextFormatString = "HH:mm:ss";
+            // X轴刻度按照日期显示
+            line.IsXAxisTextDateTimeFormat = true;
+
+            // 轴跟随数据变化
+            line.IsAxisFollowData = true;
+            // Y轴范围
+            line.YMin = 0;
+            line.YMax = 80;
+
+            // 数据模拟
+            Random rand = new Random();
+            List<Point> points = new List<Point>();
+            double y = 10;
+            while (dtStart < DateTime.Now)
+            {
+                var ran = rand.Next(-150, 150);
+                if (ran > 0)
+                {
+                    y -= 1;
+                }
+                else
+                {
+                    y += 1;
+                }
+                points.Add(new Point(dtStart.ToOADate(), y));
+                dtStart = dtStart.AddMilliseconds(500);
+            }
+            line.DrawLine(points);
+
+            code.Text = @"    // 设置X轴开始显示范围
+    DateTime dtStart = DateTime.Now.AddMinutes(-3);
+    line.XMin = dtStart.ToOADate();
+    line.XMax = DateTime.Now.AddSeconds(60).ToOADate();
+    // 设置X轴刻度文本格式
+    line.XAxisTextFormatString = ""HH: mm: ss"";
+    // X轴刻度按照日期显示
+    line.IsXAxisTextDateTimeFormat = true;
+
+    // 轴跟随数据变化
+    line.IsAxisFollowData = true;
+    // Y轴范围
+    line.YMin = 0;
+    line.YMax = 80;
+
+    // 数据模拟
+    Random rand = new Random();
+    List<Point> points = new List<Point>();
+    double y = 10;
+    while (dtStart < DateTime.Now)
+    {
+        var ran = rand.Next(-150, 150);
+        if (ran > 0)
+        {
+            y -= 1;
+        }
+        else
+        {
+            y += 1;
+        }
+        points.Add(new Point(dtStart.ToOADate(), y));
+        dtStart = dtStart.AddMilliseconds(500);
+    }
+    line.DrawLine(points);";
+
+            xaml.Text = @"<Page x:Class=""WindChart.ApplyDemo.Pages.Linegrams.CodebehindDemo1Page""
+      xmlns = ""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+      xmlns: x = ""http://schemas.microsoft.com/winfx/2006/xaml""
+      xmlns: mc = ""http://schemas.openxmlformats.org/markup-compatibility/2006""
+      xmlns: d = ""http://schemas.microsoft.com/expression/blend/2008""
+      xmlns: local = ""clr-namespace:WindChart.ApplyDemo.Pages.Linegrams""
+      xmlns: windchart = ""clr-namespace:WindChart;assembly=WindChart""
+      mc: Ignorable = ""d""
+      d: DesignHeight = ""450"" d: DesignWidth = ""800""
+      Title = ""CodebehindDemo1Page"" >
+     <Grid>
+        < Border Padding = ""24"" Background = ""WhiteSmoke"" >
+            <windchart:Linegram x:Name = ""line"" />
+         </Border >
+     </Grid >
+ </Page > ";
+        }
+    }
+}
